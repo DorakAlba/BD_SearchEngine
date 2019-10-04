c01634bbc3bc07412440ac1a2b7356780625ccbd
vAD_lv <derikaVB@gmail.com>
2019-10-04 13:34:27 +0300

    Count words in full folder and in single file

--- scala/src/main/scala/MainClass.scala
+++ scala/src/main/scala/MainClass.scala
@@ -1,14 +1,8 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
-//import io.circe._
-//import io.circe.parser._
-// import io.circe._
-// import io.circe.parser._
-//import cats.syntax.either._
-//import spark.implicits._
+import scala.util.matching.Regex
 import org.apache.spark._
 import org.apache.spark.sql._
-import org.apache.log4j._
 import org.apache.spark.sql.SparkSession
 
 object MainClass{
@@ -16,7 +10,7 @@ object MainClass{
     val conf = new SparkConf().setAppName("Sample App")
     conf.setMaster("local")
     val sc = new SparkContext(conf)
-    val sqlContext = new SQLContext(sc)
+    //val sqlContext = new SQLContext(sc)
     val mySpark = SparkSession
       .builder()
       .appName("Spark SQL basic example")
@@ -24,26 +18,59 @@ object MainClass{
       .getOrCreate()
     import mySpark.implicits._
 
-    val inputPath = "/Users/V/Desktop/scala/src/main/scala/AA_wiki_00"
+    val inputPath = "/Users/V/Desktop/BD_ass/files/AA_wiki_00"
+    val inputFolder = "/Users/V/Desktop/BD_ass/files"
+    val outputPath = "/Users/V/Desktop/BD_ass/Test3"
+    val outputFolder = "/Users/V/Desktop/BD_ass/Test6All"
 
-    val readExample = mySpark.read.option("multiLine", true)
+    /*val readExample = mySpark.read.option("multiLine", true)
       .option("mode", "PERMISSIVE")
-      .json(inputPath)
+      .json(inputPath)*/
+    val regex2 = "(.*\\p{Punct}.*)" //регулярное выражение для удаления всех слов с пунктуацией
 
-    readExample.printSchema()
-    readExample.createOrReplaceTempView("Files")
-    val data=mySpark.sql("select * from Files")
-    data.show()
+    /*читаем один файл, который состоит из нескольких элементов, считаем кол-во слов
+    val readFull = mySpark.read.json(inputPath)
+    readFull.printSchema()
+    readFull.createOrReplaceTempView("Files")
+    val data=mySpark.sql("select text from Files")
+    //val d = readFull.select('text).collect().map(rows => rows.getString(0))//.map(line => line.replaceAll(regex2, ""))
+    val ds = data.map(rows => rows.getString(0)).map(line => line.replaceAll(regex2, "")).rdd
+      val counts = ds.flatMap(line => line.split(" "))
+      //.filter(word => !(word matches regex2))
+        .map(word => (word, 1))
+        .reduceByKey(_ + _)
+        .sortBy(- _._2)
+        .map(f => f._1 +"\t"+ f._2)
+    counts.saveAsTextFile(outputPath)*/
 
-    /*val df = sqlContext.read.json(inputPath)
+    //считываем все файлы из папки в один dataset и считаем слова
+    //по сути нам надо создать такой файл для каждой строки/документа отдельно
+    //и создать вектор считающий в скольких документах содержится слово
+    val readAll = mySpark.read.json(inputFolder)
+    readAll.printSchema()
+    readAll.createOrReplaceTempView("Files")
+    val data2=mySpark.sql("select text from Files")
+    //val d = readFull.select('text).collect().map(rows => rows.getString(0))//.map(line => line.replaceAll(regex2, ""))
+    val ds2 = data2.map(rows => rows.getString(0)).rdd
+    val counts2 = ds2.flatMap(line => line.split(" "))
+      .filter(word => !(word matches regex2)) //удаляем все слова с пунктуацией, если найдете норм способ удалить только пунктуацию будет круто, но необязательно
+      //.map(line => line.replaceAll(regex2,"")) //работает не очень, вставляет пустые символы
+      //.filter(word => !(word matches ""))
+      .map(word => (word, 1))
+      .reduceByKey(_ + _)
+      .sortBy(- _._2)
+      .map(f => f._1 +"\t"+ f._2)
+    counts2.saveAsTextFile(outputFolder)
+
+    /* другой способ
+    val sqlContext = new SQLContext(sc)
+    val df = sqlContext.read.json(inputPath)
     df.printSchema()
     df.registerTempTable("JSONdata")
     val data=sqlContext.sql("select title from JSONdata")
-    c
     sc.stop*/
-
     //val inputPath = args(0)
-    //val rawJson: String = """{"foo": "bar","baz": 123,"list of stuff": [ 4, 5, 6 ]}"""
+    //val rawJson: String = {"foo": "bar","baz": 123,"list of stuff": [ 4, 5, 6 ]}"""
     //val parseResult = parse(rawJson).getOrElse(Json.Null)
     //println(parseResult)
   }
