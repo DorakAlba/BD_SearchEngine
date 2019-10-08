a5dbb4ac4e03d1dd8e929d41bdeb15a88032d9e1
vAD_lv <derikaVB@gmail.com>
2019-10-08 16:08:52 +0300

    Final code update

--- Indexer.scala
+++ Indexer.scala
@@ -51,7 +51,7 @@ object Indexer{
       .map(line => line.replaceAll(pat3, ""))
       .map(word => (word, 1))
       .reduceByKey(_ + _)
-    // .sortBy(- _._2)
+      //.sortBy(- _._2) убираем для оптимизации
       .map(f => f._1 +"\t"+ f._2)
     //vocab.saveAsTextFile(outputFolder)
 
@@ -67,7 +67,7 @@ object Indexer{
     val dss2 = dss.withColumn("text", explode($"text"))
       .groupBy("id","text")
       .count()
-    //  .sort(asc("id"),desc("count"))
+      //.sort(asc("id"),desc("count")) убираем для оптимизации
 
     //dss2.printSchema()
 
